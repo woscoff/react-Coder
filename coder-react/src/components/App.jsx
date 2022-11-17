15172f670f4fd84b81074e170683ce4371593800
woscoff <73312435+woscoff@users.noreply.github.com>
2022-11-17 20:52:26 -0300

    17/11

---
+++ coder-react/src/components/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Navbar/Navbar';
+import Form from './Form/Form';
+const App = () => {
+
+  return (
+    <>
+      <Navbar/>
+      <Form busqueda={"Buscar productos"}/>
+    </>
+  );
+}
+
+export default App;
---
+++ coder-react/src/components/Navbar/Categorias/Categorias.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Categorias = () => {
+    return (
+        <div>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Hogar</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Casa</a>
+              </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Categorias;
+
